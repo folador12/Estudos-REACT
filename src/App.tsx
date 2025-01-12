b0a08cf6162a01191913a5216055ed2140b5f5a4
Luiz Folador <luizantoniofoladorfilho@gmail.com>
2025-01-12 16:30:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='w-full flex flex-col '>
+      <h1 className='text-lg text-red-700 text-center font-semibold'>
+        Safadinho!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
