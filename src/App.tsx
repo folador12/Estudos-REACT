4b15e6757d1886ef048b0e003cf34f1a7bfde3db
Luiz Folador <luizantoniofoladorfilho@gmail.com>
2025-01-12 22:38:28 -0300

    feat: shadcn/ui and function cn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className='w-full flex flex-col '>
       <h1 className='text-lg text-red-700 text-center font-semibold'>
-        Safadinho!!
+        {count}
       </h1>
+
+      <Button
+        onClick={() => setCount(count + 1)}
+        className='w-1/2 mx-auto mt-4'
+        variant='outline'
+        size='lg'
+      >
+        Contar
+      </Button>
     </div>
   );
 }
